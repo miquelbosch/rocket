a2ee4692105adf1d07b31f1dd07584ffbe162e3f
Miquel Bosch <miquel.bosch.cortes@gmail.com>
2021-02-13 10:27:51 +0100

    initial commit

---
+++ Rocket/Rocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rocket
+//
+//  Created by Miquel Bosch on 13/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
